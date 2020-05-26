ec716bc684a5a12cc9261b170861baa24f6bcbb4
gouxuefeng <gouxuefeng@ysstech.com>
2020-05-26 19:59:11 +0800

    11

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,22 @@
+const webpack = require('webpack')
 module.exports = {
+  configureWebpack: {
+
+    plugins: [
+
+      new webpack.ProvidePlugin({
+
+        $:"jquery",
+
+        jQuery:"jquery",
+
+        "windows.jQuery":"jquery"
+
+      })
+
+    ]
+
+  },
   presets: [
     '@vue/cli-plugin-babel/preset'
   ]
